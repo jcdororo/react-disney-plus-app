bdcd42f697edda733cd3094b178ec8da985e9492
jc <wnwlcks123@gmail.com>
2023-09-17 14:42:27 +0900

    네비게이션 컴포넌트 스크롤 구현하기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 
-const Nav = () => {
+
+const Nav = () => {  
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    })
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+  }, [])
+  
+
   return (
-    <NavWrapper>
+    <NavWrapper show={show}>
       <Logo>
         <img 
           alt='Disney Plus Logo'
           src='/images/logo.svg'
+          onClick={() => (window.location.href = '/')}
         />
       </Logo>
     </NavWrapper>
@@ -22,7 +41,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${props => props.show ? "#090b13" : "transparent"};
   display: flex;
   justify-content: space-between;
   align-items: center;
