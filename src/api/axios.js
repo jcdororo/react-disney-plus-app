a29d5f4f372a54e7a24598693fb85989c6fa8b30
jc <wnwlcks123@gmail.com>
2023-09-17 13:56:59 +0900

    Disney Plus 애플리케이션 전체 구조 생성하기

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "c68e8750cd621137bc5c412f55f73589",
+    language: "ko-KR"
+  }
+});
+
+
+export default instance;
---
+++ src/api/request.js
@@ -0,0 +1,12 @@
+const requests = {
+  fetchNowPlaying: "movie/now_playing",
+  fetchTrending: "/trending/all/week",
+  fetchTopRated: "/movie/top_rated",
+  fetchActionMovies: "/discover/movie?with_genres=28",
+  fetchComedyMovied: "/discover/movie?with_genres=35",
+  fetchHorrorMovies: "/discover/movie?with_genres=27",
+  fetchRomadMovies: "/discover/movie?with_genres=10749",
+  fetchDocumentaryMovies: "/discover/movie?with_genres=99"
+}
+
+export default requests;
