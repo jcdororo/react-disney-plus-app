9d1bda99a9482a0b4c3181dee3ba20fef1797527
jc <wnwlcks123@gmail.com>
2023-09-17 21:27:47 +0900

    Row 컴포넌트 CSS 작성하기

--- src/api/request.js
+++ src/api/request.js
@@ -3,10 +3,10 @@ const requests = {
   fetchTrending: "/trending/all/week",
   fetchTopRated: "/movie/top_rated",
   fetchActionMovies: "/discover/movie?with_genres=28",
-  fetchComedyMovied: "/discover/movie?with_genres=35",
+  fetchComedyMovies: "/discover/movie?with_genres=35",
   fetchHorrorMovies: "/discover/movie?with_genres=27",
-  fetchRomadMovies: "/discover/movie?with_genres=10749",
-  fetchDocumentaryMovies: "/discover/movie?with_genres=99"
+  fetchRomanceMovies: "/discover/movie?with_genres=10749",
+  fetchDocumentaries: "/discover/movie?with_genres=99"
 }
 
 export default requests;
