57e94907016e9d18e53c988b9f565fa4cdbbd97a
jc <wnwlcks123@gmail.com>
2023-09-18 21:37:23 +0900

    swiper 모듈을 이용한 터치 슬라이드 구현하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,17 @@ import React, { useCallback, useEffect, useState } from 'react'
 import './Row.css'
 import MovieModal from './MovieModal';
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper style
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
+import styled from 'styled-components';
+
+
 const Row = ({title, id, fetchUrl}) => {
   const [movies, setMovies] = useState([])
   const [modalOpen, setModalOpen] = useState(false)
@@ -27,51 +38,52 @@ const Row = ({title, id, fetchUrl}) => {
 
 
   return (
-    <div>
+    <Container>
       <h2>{title}</h2>
-      <div className='slider'>
-        <div className='slider__arrow-left'>
-          <span 
-          className='arrow'
-          onClick={() => {
-            document.getElementById(id).scrollLeft -= (window.innerWidth - 80);
-          }}
-          >
-            {"<"}
-          </span>
-        </div>
-        <div id={id} className="row__posters">
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} //loop 기능을 사용할 것인지
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} //페이지 버튼 보이게 할지
+      >
+
+        <Content id={id}>
           {movies.map((movie) => (
-              <img
+            <SwiperSlide>
+              <Wrap>
+
+                <img
                 key={movie.id}
                 className="row__poster"
                 src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
                 alt={movie.name}
                 onClick={() => handleClick(movie)}
-              />
-              ))}
-        </div>
-        <div className='slider__arrow-right'>
-          <span 
-          className='arrow'
-          onClick={() => {
-            document.getElementById(id).scrollLeft += (window.innerWidth - 80);
-          }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+                />
+              </Wrap>
+            </SwiperSlide>
+            ))}
+        </Content>
+      </Swiper>
 
       {modalOpen &&
         <MovieModal
-          {...movieSelected}
-          setModalOpen={setModalOpen}
+        {...movieSelected}
+        setModalOpen={setModalOpen}
         />
       }
-
-    </div>
+      
+    </Container>
   )
 }
 
 export default Row
+
+
+const Container = styled.div`
+  padding: 0 0 26px;
+`;
+
+const Content = styled.div``;
+
+const Wrap = styled.div``;
