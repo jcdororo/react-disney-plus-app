43113654a34a256fb4316293a12fa17cfd1a3632
jc <wnwlcks123@gmail.com>
2023-09-18 16:29:47 +0900

    useLocation을 이용한 검색 페이지 구현하기

---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,37 @@
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+
+const SearchPage = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if(searchTerm){
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm])
+
+  const fetchSearchMovie = async () => {
+    try {
+      const response = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      setSearchResults(response.data.result);
+      console.log(response)
+    } catch (error) {
+      console.log(error);
+    } 
+  }
+
+  return (
+    <div>SearchPage</div>
+  )
+}
+
+export default SearchPage
